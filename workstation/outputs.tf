b8ac7f558c091047dbd4739877269e4f7170b538
meerjan <janushaik9191@gmail.com>
2025-04-28 23:09:50 +0530

    work station requirements

---
+++ workstation/outputs.tf
@@ -0,0 +1,3 @@
+output "public_ip" {
+  value = module.workstation.public_ip
+}
---
+++ workstation/variables.tf
@@ -0,0 +1,9 @@
+variable "sg_id" {
+  type        = string
+  default     = "sg-0d0488aad8797fa18"
+}
+
+variable "public_subnet_id" {
+    type = string
+    default = "subnet-0d379f052e48ae9e0"
+}
